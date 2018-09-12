ff2df25d2084c1b0ee15525b350d33b8202268ea
Weijiang Li <wl13@illinois.edu>
2018-09-12 18:40:33 -0500

    good

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -17,6 +17,7 @@ public class SolveMaze {
      */
 
     public static void main(final String[] unused) {
+
         /*
          * Create a new 10 x 10 maze. Feel free to change these values.
          */
@@ -35,12 +36,20 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
-        }
+            while (maze.isFinished() == false) {
+                maze.turnLeft();
+                while (maze.canMove() == false) {
+                    maze.turnRight();
+                }
+                maze.move();
+            }
+            if (maze.isFinished()) {
+                System.out.println("You solved the maze!");
+            } else {
+                System.out.println("Try again!");
 
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
-        } else {
-            System.out.println("Try again!");
+
+            }
         }
     }
 }
